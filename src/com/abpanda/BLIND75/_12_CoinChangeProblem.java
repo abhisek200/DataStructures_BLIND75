7456542317c8c880227a48fab5a2ea024ddbc1d4
abhisek200_ <pandaabhisek200@gmail.com>
2024-11-20 08:34:27 +0530

    12 and 13

---
+++ src/com/abpanda/BLIND75/_12_CoinChangeProblem.java
@@ -0,0 +1,28 @@
+package com.abpanda.BLIND75;
+
+import java.util.Arrays;
+
+public class _12_CoinChangeProblem {
+    public static void main(String[] args) {
+        int coins[] = {1, 3, 5, 6};
+        int amount = 11;
+        System.out.println("Resultant amount : " + coinChangeDP(coins, amount));
+    }
+
+    public static int coinChangeDP(int coins[], int amount) {
+        int amt[] = new int[amount + 1];
+        Arrays.fill(amt, amount + 1); // if we cannot create a amount we return -1
+        amt[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j]) {
+                    amt[i] = Math.min(amt[i], 1 + amt[i - coins[j]]);
+                }
+            }
+        }
+        if (amt[amount] < amount + 1) return amt[amount];
+        else {
+            return -1;
+        }
+    }
+}
---
+++ src/com/abpanda/BLIND75/_13_LongestIncreasingSubsequence.java
@@ -0,0 +1,26 @@
+package com.abpanda.BLIND75;
+
+import java.util.Arrays;
+
+public class _13_LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        int num[] = {10, 7, 8, 5, 16, 27, 9};
+        System.out.println("Longest Increasing Subsequence Length : " + longestIncreasingSubsequence(num));
+    }
+
+    public static int longestIncreasingSubsequence(int[] nums) {
+        int lis[] = new int[nums.length];
+        Arrays.fill(lis, 1);
+        int max = -1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    lis[i] = Math.max(lis[i], 1 + lis[j]); // recurrence relation
+                    max = Math.max(max, lis[i]);
+                }
+            }
+        }
+        return max;
+
+    }
+}
